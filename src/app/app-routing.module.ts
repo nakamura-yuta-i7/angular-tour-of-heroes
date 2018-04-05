5cb752c7236adb5e1fd9d87ee5430a9afd887ffc
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2018-04-05 22:47:36 +0900

    hash on

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [
+    // RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
